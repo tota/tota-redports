fe8d9732ee43abf79e568bd2ac8acbf9040e2120
TAKATSU Tomonari <tota@FreeBSD.org>
2014-02-16 07:22:14 +0000

    - Fix to stage
    git-svn-id: https://svn.redports.org/tota@23160 2e334835-ea15-e111-ad42-00505682000c

--- print/py-reportlab3/files/patch-docs-genAll.py
+++ print/py-reportlab3/files/patch-docs-genAll.py
@@ -1,15 +1,11 @@
 --- docs/genAll.py.orig	2014-02-14 23:22:37.000000000 +0900
-+++ docs/genAll.py	2014-02-16 11:46:38.000000000 +0900
-@@ -1,5 +1,5 @@
++++ docs/genAll.py	2014-02-16 16:20:21.000000000 +0900
+@@ -1,6 +1,7 @@
  #!/bin/env python
 -import os, sys, traceback
 +import os, sys, traceback, site
  def _genAll(verbose=1):
++    site.addsitedir("%%STAGEDIR%%%%PYTHON_SITELIBDIR%%")
      from reportlab.lib.testutils import setOutDir
      setOutDir(__name__)
-@@ -43,4 +43,5 @@
-     elif not os.path.isabs(d):
-         d = os.path.abspath(d)
-     sys.path.insert(0,os.path.dirname(d))
-+    site.addsitedir("%%STAGEDIR%%%%PYTHON_SITELIBDIR%%")
-     _genAll(verbose)
+     from reportlab.lib.testutils import testsFolder
